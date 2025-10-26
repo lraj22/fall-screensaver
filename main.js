d5f73967bc505b0ac2ae1db863730c950bff4889
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-10-25 20:22:07 -0700

    Make 10 leaves fall separately

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
 // main.js - runs the main logic for fall screensaver
 
-document.querySelector("img").addEventListener("animationiteration", function () {
-	this.style.left = (Math.random() * window.innerWidth - 50) + "px";
-});
+let total = 10;
+
+for (let i = 0; i < total; i++) {
+	let img = document.createElement("img");
+	img.src = "https://www.clipartqueen.com/image-files/red-lobed-fall-clipart-leaf.png";
+	img.alt = "leaf";
+	document.body.appendChild(img);
+	img.style.animationDuration = ((Math.random () * 3) + 2) + "s";
+	img.style.left = ((Math.random() * window.innerWidth) - 50) + "px";
+	
+	img.addEventListener("animationiteration", function () {
+		this.style.left = ((Math.random() * window.innerWidth) - 50) + "px";
+		this.style.top = "-100px";
+	});
+}
 
 console.log("ready!");
