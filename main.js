ba1557816f1a7428570b9a2d92f8ef567970f08f
lraj22 <63673413+lraj22@users.noreply.github.com>
2025-10-25 22:00:09 -0700

    Get ready for leaf clicking + add bg
    I tried a lot of things with adding exploding stars and stuff when the leaf is clicked but nothing works yet. Hopefully next commit something will work!

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 TweenLite.set("#container", {
 	"perspective": 600,
 });
-let total = 20;
+let total = 30;
 let w = window.innerWidth;
 let h = window.innerHeight;
 
@@ -12,10 +12,10 @@ function random(min,max) {return min+Math.random()*(max-min)};
 function animate (leaf){
 	gsap.to(leaf, {
 		"duration": random(6,15),
-		"y": h+100,
+		"y": h + 100,
 		"ease": Linear.easeNone,
 		"repeat": -1,
-		"delay": -15
+		"delay": -15,
 	});
 	gsap.to(leaf, {
 		"duration": random(4,8),
@@ -37,21 +37,27 @@ function animate (leaf){
 }
 
 for (let i = 0; i < total; i++) {
-	let img = document.createElement("img");
-	img.src = "https://www.clipartqueen.com/image-files/red-lobed-fall-clipart-leaf.png";
-	img.alt = "leaf";
-	img.style.width = ((Math.random() * 30) + 70) + "px";
-	gsap.set(img, {
+	let leaf = document.createElement("img");
+	leaf.src = "https://www.clipartqueen.com/image-files/red-lobed-fall-clipart-leaf.png"; // leaf png
+	leaf.alt = "leaf";
+	leaf.style.width = ((Math.random() * 30) + (Math.min(w, h) / 20)) + "px";
+	leaf.id = "leaf-" + i;
+	gsap.set(leaf, {
 		"attr": {
 			"class": "dot",
 		},
 		"x": random(0, w),
-		"y": random(-200, -150),
+		"y": random(-150, -100),
 		"z": random(-200, 200),
 	});
 	
-	document.body.appendChild(img);
-	animate(img);
+	leaf.onclick = function () {
+		console.log("You clicked on " + leaf.id);
+		alert("I love fall!");
+	};
+	
+	document.body.appendChild(leaf);
+	animate(leaf);
 }
 
 console.log("ready!");
